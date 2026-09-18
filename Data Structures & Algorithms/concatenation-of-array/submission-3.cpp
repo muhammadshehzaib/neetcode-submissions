d0009e07a589124d50da9789e5826006b681f00a
Muhammad Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2026-09-19 02:14:19 +0500

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans (nums.size()*2,0);
+        for(int i=0;i<nums.size()*2;i++){
+            ans[i] = nums[i%nums.size()];
+        }
+        return ans;
+    }
+};
